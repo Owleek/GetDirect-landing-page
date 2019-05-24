0b2bec2d42e4def38e6437eaca0d48ac8f565c7e
Artemiy <owleek@mail.ru>
2019-05-24 17:18:17 +0300

    owlCrousel

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,8 +1,25 @@
 $(document).ready(function() {
+	$('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:20,
+        dots:false,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
 	$('dl.tabsComponent').on('click', 'dt', function() {
 		$(this)
-			.siblings().removeClass('active').end()
-			.next().addClass('active').end()
-			.addClass('active')
-	});				
+
+			.next().toggleClass('active').end()
+			.toggleClass('active')
+	});	
 });
