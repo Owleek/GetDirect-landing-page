607cf2cd7366dc1a45ee5c8a0055225dda1e844c
Artemiy <owleek@mail.ru>
2019-05-24 13:03:51 +0300

    productPage

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-	// do something
+	$('dl.tabsComponent').on('click', 'dt', function() {
+		$(this)
+			.siblings().removeClass('active').end()
+			.next().addClass('active').end()
+			.addClass('active')
+	});				
 });
